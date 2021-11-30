463b3285c95a859796fc0dc4efbf119c801dbabf
naeunkim1227 <kne0140@daum.net>
2021-11-30 15:51:31 +0900

    21.11.30

---
+++ naeun-test/src/main/java/com/tikitaka/service/RedisService.java
@@ -0,0 +1,24 @@
+package com.tikitaka.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisService {
+	
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+	
+	
+	public void getRedisStringValue(String key) {
+		
+		ValueOperations<String, String> svo = stringRedisTemplate.opsForValue();
+		System.out.println(key);
+		System.out.println(svo.get(key));
+		
+		
+	}
+	
+}
